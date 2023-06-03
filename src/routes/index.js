627ba8c054d20dd3e67ebb90999eebf26fa7f43a
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-06-03 17:27:38 -0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,16 @@ import express from "express";
 import produto from "./produtosRoutes.js";
 
 const routes = (app) => {
+  app.route("/").get((req, res) => {
+    res.status(200).send({ tiulo: "Lovelier Joais" });
+  });
+
   // Configuração do cabeçalho Access-Control-Allow-Origin
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   });
 
-  app.route("/").get((req, res) => {
-    res.status(200).send({ tiulo: "Lovelier Joais" });
-  });
   app.use(express.json(), produto);
 };
 
