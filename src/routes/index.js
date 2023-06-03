a35500586f1450116cd55107cc44e4a048f4a664
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-06-03 16:35:40 -0300

    cabeçalho Access-Control-Allow-Origin

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,12 @@ import express from "express";
 import produto from "./produtosRoutes.js";
 
 const routes = (app) => {
+  // Configuração do cabeçalho Access-Control-Allow-Origin
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  });
+
   app.route("/").get((req, res) => {
     res.status(200).send({ tiulo: "Lovelier Joais" });
   });
